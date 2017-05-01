533c875f8e71e615b89f193a8afc8cbef251057d
Jason Brown and Koji Nakagawa <koji@>
2017-05-01 14:27:25 -0700

    add about, about you, contact, FAQ basic page, add theme

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -244,7 +244,21 @@
  *   );
  * @endcode
  */
-$databases = array();
+$databases = array (
+  'default' => 
+  array (
+    'default' => 
+    array (
+      'database' => 'recipe-blog',
+      'username' => 'recipe-blog',
+      'password' => 'password',
+      'host' => '127.0.0.1',
+      'port' => '8889',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
 
 /**
  * Access control for update.php script.
@@ -276,7 +290,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = '';
+$drupal_hash_salt = 'b9pEWW0-qwnTZ7RrkdKjG2Bkv3Z8y76EJ-qZ7CvoSuU';
 
 /**
  * Base URL (optional).
